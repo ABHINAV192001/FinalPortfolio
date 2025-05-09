f84be50943cdb72dc9e2710b894699feb1de7cb0
Abhinav Sahu <abhinavsahu120@gmail.com>
2025-05-09 16:07:09 +0530

    Update Skills.js

--- src/files/Skills.js
+++ src/files/Skills.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Projects from "./Projects";
+
 export default function Skills() {
   return (
     <div className="container text-center py-5" id="skills">
@@ -10,10 +11,26 @@ export default function Skills() {
           <div className="card shadow-sm p-4">
             <h4 className="fw-bold">Technical Skills</h4>
             <ul className="list-unstyled">
-              <li className="fs-5 fw-bold"><i className="fas fa-code text-primary me-2"></i> Java <span className="fw-normal">(Spring Boot)</span></li>
-              <li className="fs-5 fw-bold"><i className="fas fa-database text-success me-2"></i> SQL (MySQL, PostgreSQL)<span className="fw-normal"></span></li>
-              <li className="fs-5 fw-bold"><i className="fab fa-html5 text-danger me-2"></i> HTML (CSS, Bootstrap)<span className="fw-normal"></span></li>
-              <li className="fs-5 fw-bold"><i className="fab fa-js text-warning me-2"></i> JavaScript (React, ES6+)<span className="fw-normal"></span></li>
+              <li className="fs-5 fw-bold">
+                <i className="fas fa-code text-primary me-2"></i> 
+                Java (Spring Boot Learning)<span className="fw-normal"></span>
+              </li>
+              <li className="fs-5 fw-bold">
+                <i className="fas fa-database text-success me-2"></i> 
+                SQL (MySQL, PostgreSQL)<span className="fw-normal"></span>
+              </li>
+              <li className="fs-5 fw-bold">
+                <i className="fab fa-html5 text-danger me-2"></i> 
+                HTML, CSS, Bootstrap
+              </li>
+              <li className="fs-5 fw-bold">
+                <i className="fab fa-js text-warning me-2"></i> 
+                JavaScript (React, ES6+)<span className="fw-normal"></span>
+              </li>
+              <li className="fs-5 fw-bold">
+                <i className="fas fa-vials text-info me-2"></i> 
+                Testing (Manual Testing, Automation Testing with Java)<span className="fw-normal"></span>
+              </li>
             </ul>
           </div>
         </div>
@@ -22,7 +39,6 @@ export default function Skills() {
       <div id="Projects">
         <Projects />
       </div>
-
     </div>
   );
 }
